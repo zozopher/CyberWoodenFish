821718c6498834a2ebc196f97a96da52dc7aaf24
你的姓名 <2456230898@qq.com>
2025-12-14 18:50:40 +0800

    first commit

---
+++ animateTry1.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector('.knockBtn')
+let container = document.querySelector('#msgContainer')
+btn.addEventListener('click',function () {
+    const newMsg = document.createElement('span')
+    newMsg.className = 'msg'
+    newMsg.textContent = '功德 +1'
+    container.appendChild(newMsg)
+    let msg = document.querySelectorAll('.msg')
+    setTimeout(() => {
+        msg.forEach(m => {
+            m.classList.remove('fin')
+            m.classList.add('mid')})
+    }, 50);
+    setTimeout(() => {
+        msg.forEach(m => {
+            m.classList.remove('mid')
+            m.classList.add('fin')})
+    }, 1050);
+    setTimeout(() => {
+        newMsg.remove()
+    }, 2000);
+})
