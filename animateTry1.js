5032dd5ce8c81d77f164e6f05f7f7b9cc392175a
你的姓名 <2456230898@qq.com>
2025-12-16 17:43:59 +0800

    新增了木鱼棍与频繁敲击提示

--- animateTry1.js
+++ animateTry1.js
@@ -1,21 +1,36 @@
 let btn = document.querySelector('.knockBtn')
 let container = document.querySelector('#msgContainer')
 btn.addEventListener('click',function () {
+    // 获取棍子，控制动画类名
+    const stk = document.querySelector('.stickBox')
+    stk.classList.add('animate')
+    setTimeout(() => {
+        stk.classList.remove('animate')
+    }, 100);
+    // 每次敲击时，新创建一个功德提示元素
     const newMsg = document.createElement('span')
     newMsg.className = 'msg'
     newMsg.textContent = '功德 +1'
     container.appendChild(newMsg)
     let msg = document.querySelectorAll('.msg')
+    // 快速连点提示
+    if (msg.length > 5) {
+        document.querySelector('.tooFast').classList.add('show')
+        setTimeout(() => {
+            document.querySelector('.tooFast').classList.remove('show')
+        }, 3000);
+    }
+    // 添加淡入淡出效果
     setTimeout(() => {
         msg.forEach(m => {
             m.classList.remove('fin')
             m.classList.add('mid')})
-    }, 50);
+    }, 50); // 这里是在等浏览器将新元素渲染完毕
     setTimeout(() => {
         msg.forEach(m => {
             m.classList.remove('mid')
             m.classList.add('fin')})
-    }, 500);
+    }, 550);
     setTimeout(() => {
         newMsg.remove()
     }, 1050);
